735433a9f5b70d077083f3d24863697989a257e5
ethankyzivat <ethan_kyzivat@brown.edu>
2018-10-04 13:03:49 -0400

    Added waterindex old 2

--- waterindex.m
+++ waterindex.m
@@ -7,8 +7,6 @@ cir=im2single(cir);
 switch waterIndex
     case 'NDWI'
         cir_index=(cir(:,:,3)-cir(:,:,1))./(cir(:,:,3)+cir(:,:,1));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level-.02);
     case 'BNDWI'
 %         woij
     case 'MNDWI'
@@ -16,16 +14,10 @@ switch waterIndex
     case 'NDVI'
 %         cir_ndwi=(cir(:,:,2)-cir(:,:,1))./(cir(:,:,2)+cir(:,:,1));
         cir_index=(.5*(cir(:,:,2)+cir(:,:,3))-cir(:,:,1))./(.5*(cir(:,:,2)+cir(:,:,3))+cir(:,:,1));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level-.02);    
     case 'WI'
         cir_index=(cir(:,:,3)-cir(:,:,1));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
     case 'MAXNDWI'
         cir_index=(max(cir(:,:,2), cir(:,:,3))-cir(:,:,1))./(max(cir(:,:,2), cir(:,:,3))+cir(:,:,1));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
     case 'SPAN'
         u1=[.353, .522, .463]; %Yukon color
         u2=[.106, .192, .212]; %Dark color
@@ -34,39 +26,25 @@ switch waterIndex
         bw=imbinarize(cir_index, level);
     case 'MAXWI'
         cir_index=(max(cir(:,:,2), cir(:,:,3))-cir(:,:,1));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
     case 'MINNDWI'
         cir_index=(min(cir(:,:,2), cir(:,:,3))-cir(:,:,1))./(min(cir(:,:,2), cir(:,:,3))+cir(:,:,1));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
     case 'MINWI'
         cir_index=(min(cir(:,:,2), cir(:,:,3))-cir(:,:,1));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
     case 'BNDWI_3_100'
         cir_index=(100*cir(:,:,3)-cir(:,:,1))./(100*cir(:,:,3)+cir(:,:,1));
 %         cir_ndwi=im2single((cir_ndwi+abs(min(min(cir_ndwi))))./(max(max(cir_ndwi))...
 %             +abs(min(min(cir_ndwi)))));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
     case 'BNDWI_1_100'
         cir_index=(cir(:,:,3)-100*cir(:,:,1))./(cir(:,:,3)+100*cir(:,:,1));
 %         cir_ndwi=im2double((cir_ndwi+abs(min(min(cir_ndwi))))./(max(max(cir_ndwi))...
 %             +abs(min(min(cir_ndwi)))));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
     case 'BNDWI_1_10'
         cir_index=(cir(:,:,3)-10*cir(:,:,1))./(cir(:,:,3)+10*cir(:,:,1));
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
     case 'AWEI'
         cir_index=1*cir(:,:,3)-2.5*cir(:,:,2)-3*cir(:,:,1);
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
+
     case 'IR'
         cir_index=cir(:,:,1);
-        level=graythresh(cir_index(~NoValues));
-        bw=imbinarize(cir_index, level);
+
 
 end

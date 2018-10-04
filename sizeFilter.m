b07d88fecc416f1525aff0ce2fe1d463d2ad06ce
ethankyzivat <ethan_kyzivat@brown.edu>
2018-10-04 14:20:16 -0400

    added sizeFilter.m

---
+++ sizeFilter.m
@@ -0,0 +1,15 @@
+function bw_out=sizeFilter(bw, minSize)
+% filters regions in binary image below minSize (inclusive)
+% uses 8-connectivity
+% output is also binary
+
+Lold=bwlabel(bw, 8);
+stats=regionprops(bw, 'Area');lstats=length(stats);
+allowableAreaIndexes = ([stats.Area] >= minSize);
+keeperIndexes = find(allowableAreaIndexes); 
+L = ismember(Lold, keeperIndexes); 
+bw_out=L>0; clear bw
+L=bwlabel(bw_out);
+stats = regionprops(bw_out, 'Area');
+fprintf('Size filter completed.  Removed %u regions.  \nNew total= %u regions.\n',...
+    lstats-length(stats), length(stats))

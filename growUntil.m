3b0795ab7c85f0093fec5791ca36c4a30de540d2
ethankyzivat <ethan_kyzivat@brown.edu>
2018-10-04 14:04:23 -0400

    V2 of growuntil V2 use alternative to unique() function and fixes mean bug

--- growUntil.m
+++ growUntil.m
@@ -1,87 +1,47 @@
-function filled=imfillNaN(bw, mask)
+function complete_region=growUntil(g, spIncl, outputImage, sp_mean, lims)
+% V2 use alternative to unique() function and fixes mean bug
+% V1 uses dynamic programming
+% (NOT) recursively dilates a superpixel 'image' (graph) for the region containing
+%labled sps 'SP_incl' (vector), using graph g (of initial water SPs) until
+% condition is met
+% lim are limits of growing (fraction) ex: .9, 1.1
+% note spIncl gives indexes to sp_mean;
+% add variance, entropy, or texture image 
+% calls SP_dil()
 
-% Function to fill in NaN values in classified, binary image
-% Only fills holes that are completely surrounded by same class (or with
-% max of two non-class pixels)
-% mask is binary mask with 1= no value (NaN value)
 
-%%%%%%%%%%%%%%%%%%%%%%%%testing
-% clear
-% close all
-% dir_in='D:\ArcGIS\FromMatlab\ClipSquares\';
-% disp('Clips in directory:')
-% tifs=cellstr(ls([dir_in, '*.tif']))
-% name_in=tifs{1}
-% img_in=[dir_in, name_in]
-% [cir, R]=geotiffread(img_in);
-% 
-% %% Set NoValue cells ==0
-% 
-% NoValues=or(cir(:,:,1)==65535 & cir(:,:,2)==65535 & cir(:,:,3)==65535,...
-%     cir(:,:,1)==0 & cir(:,:,2)==0 & cir(:,:,3)==0); %255
-% if strcmp(class(cir),'single')
-%    cir=cir.*repmat(single(~NoValues), 1, 1, 3);
-% else
-%     try
-%         cir=cir.*uint16(~NoValues);
-%     catch
-%         cir=cir.*uint8(~NoValues);
-%     end
-% end
-% %% Set NoValue cells ==NaN
-% 
-% % cir(NoValues)=NaN;
-% %% compute NDWI or similar index from 3-band image
-% [cir_index, initialMask]=waterindex(cir, 'NDWI'); 
-% % cir_index=im2uint8(cir_index);
-% try
-%     cir_index=im2uint8(cir_index+abs(min(min(cir_index))));
-% catch
-%     cir_index=single(cir_index+abs(min(min(cir_index))));
-% end
-% 
-% %% names
-% 
-% bw=initialMask;
-% mask=NoValues;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% 
-origPixels=sum(sum(bw));
-stats=regionprops(mask, 'Area', 'PixelIdxList');
-initialstats=regionprops(~bw);
-[L, N]=bwlabel(mask);
-SE=strel('disk', 1);
-% SE=ones(3,3);
-for i=1:N
-%     disp(i)
-%     disp(mean(mean(bw(ring))))
-    dil=imdilate(L==i, SE);
-    ring=dil & L==0;
-    rang=range(bw(ring));
-    if rang<=1
-%         disp('if')
-        stats(i).Fill=true;
-        stats(i).FillValue=logical(round(mean(mean(bw(ring)))));
-        n=length(stats(i).FillValue);
-        bw(stats(i).PixelIdxList)=ones(n, 1)*stats(i).FillValue;
+%%
+% init
+dil_sps=[]; %vector
+% completeRegion_old=0;
+newring.idxs=1.0;
+complete_region=spIncl; clear spIncl % units of index!
+firstTime=true;
+% loop
+while newring.idxs~=0;
+    ring.idxs=setdiff(SP_dil(g, complete_region), complete_region);
+    region.idxs=sp_mean(complete_region);
+    if firstTime
+        region.mean=mean(sp_mean(region.idxs));
     else
-%         disp('else')
-        stats(i).FillValue=0;
-        
-        % unnecessary bc Novalue cells are class as 0
-        n=length(stats(i).FillValue);
-        bw(stats(i).PixelIdxList)=ones(n, 1)*stats(i).FillValue;
+        region.mean=(region.count*region.mean+newring.count*newring.mean)/(region.count+newring.count);
     end
+    region.count=length(region.idxs);
+    bounds.a=lims(1)*region.mean;bounds.b=lims(2)*region.mean;
+%     fprintf('Region mean= %f\n', mean_region)
+%     fprintf('Length of complete_region= %d\n', length(complete_region))
+%     fprintf('Length of newring= %d\n', length(newring))
+%     fprintf('bounds= %5.0f\t%5.0f\n', bounds(1).a, bounds(1).b)
+%      std_region=std(sp_mean(SP_incl)); % single/double prob...
+
+    newring.idxs=ring.idxs(sp_mean(ring.idxs)>=bounds.a & sp_mean(ring.idxs)<=bounds.b);
+    newring.count=length(newring.idxs);
+    newring.mean=mean(sp_mean(newring.idxs));
+    complete_region=[complete_region; newring.idxs];
+    if ~firstTime & newring.count>0
+        fprintf(' %d', newring.count)
+    end
+    firstTime=false;
+%     SP_plot(complete_region, stats, size(L_all)); pause(.01)
 end
-filled=bw;
-finalPix=sum(sum(filled));
-finalstats=regionprops(~filled);
-fprintf('\tFilled all NoData cells completely surrounded by water class.\n')
-fprintf('\tNumber of cells filled:\t%d\n', finalPix-origPixels)
-fprintf('\tNumber of regions filled:\t%i\n', length(initialstats)-length(finalstats))
-%% fill
-
-
-
-% filled=logical(round(regionfill(uint8(bw), ~mask)));
+% complete_region=unique(complete_region);

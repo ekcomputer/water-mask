71acbd54f52d6a06c339e124a4357a0ab18c645b
ethankyzivat <ethan_kyzivat@brown.edu>
2018-10-10 13:41:52 -0400

    Fixed way imfillNaN counts holes

--- imfillNaN.m
+++ imfillNaN.m
@@ -8,16 +8,17 @@ function filled=imfillNaN(bw, mask)
 
 %% 
 origPixels=sum(sum(bw));
-cc1=bwconncomp(~bw); cc1.PixelIdxList=[];
+% cc1=bwconncomp(~bw); cc1.PixelIdxList=[];
 holes = imfill(bw,'holes') &~bw; % can include real islands
 holes=holes&mask; % re using name to save space.  holes now excludes real isalnds
 filled=bw;
 filled(holes==1)=1;
 finalPix=sum(sum(filled));
-cc2=bwconncomp(~filled); cc2.PixelIdxList=[];
+% cc2=bwconncomp(~filled); cc2.PixelIdxList=[];
+cc=regionprops(holes, 'Area');
 fprintf('\tFilled all NoData cells completely surrounded by water class.\n')
 fprintf('\tNumber of cells filled:\t%d\n', finalPix-origPixels)
-fprintf('\tNumber of regions filled:\t%i\n', cc1.NumObjects-cc2.NumObjects)
+fprintf('\tNumber of regions filled:\t%i\n', length(cc))
 %% fill
 
 

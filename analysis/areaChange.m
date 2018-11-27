516396e6fedad089b5c4bc7e905841fa72870938
ethankyzivat <ethan_kyzivat@brown.edu>
2018-11-27 17:20:58 -0500

    using regionprops, not L

--- analysis/areaChange.m
+++ analysis/areaChange.m
@@ -1,16 +1,16 @@
 % function to calculate area changes in lakes
 clear
     % load data
+pairs_path='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\analysis\pairs.mat';
+load(pairs_path);
     % inuvik
-im_a_pth='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\WC_20170716_S03X_Ch063v034_V1.tif';
-im_b_pth='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\WC_20170807_S01X_Ch063v034_V1.tif';
-%     % yellowknife
-% im_a_pth='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\WC_20170715_S02X_Ch083v065_V1.tif';
-% im_a_pth='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\WC_20170812_S03X_Ch083v065_V1.tif';
-%     % yellowknife 2
-% im_a_pth='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\WC_20170715_S03B_Ch084v067_V1.tif';
-% im_a_pth='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\WC_20170812_S04X_Ch084v067_V1.tif';
-
+% im_a_pth='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\WC_20170716_S03X_Ch063v034_V1.tif';
+% im_b_pth='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\WC_20170807_S01X_Ch063v034_V1.tif';
+base='D:\ArcGIS\FromMatlab\CIRLocalThreshClas\Final\';
+n=30;
+% key: inuvik =3, yellowknife 1= 26, yellowknife 2=30 (doesn't work)
+im_a_pth=[base,pairs(n).a];
+im_b_pth=[base,pairs(n).b];
 [im{1}, R_a]=geotiffread(im_a_pth);
 [im{2}, R_b]=geotiffread(im_b_pth);
 
@@ -36,7 +36,7 @@ for i=1:length(im)
     
     buf(i,1)=-NW_intrins(i,2)+R(i).YIntrinsicLimits(1); % top buffer
     buf(i,2)=-NW_intrins(i,1)+R(i).XIntrinsicLimits(1); % left buffer
-    buf(i,3)=-SE_intrins(i,2)+R(i).YIntrinsicLimits(2); % btm buffer
+    buf(i,3)=SE_intrins(i,2)-R(i).YIntrinsicLimits(2); % btm buffer
     buf(i,4)=SE_intrins(i,1)-R(i).XIntrinsicLimits(2); % right buffer
     
 %     buf(i,1)=-NW_intrins(i,2); % top buffer
@@ -61,12 +61,23 @@ end
 % im=im_a;
 % im(:,:,3)=im_b;
 
-%%
+%% stack images
 im_change=im{1}; im_change(:,:,2)= im{2};
 % d.mask=im{1}==-99 | im{2}==-99;
 d.max_water=im{1}==1 & im{2}==1;
 % d.change=im{2}-im{1} & ~d.mask;
 d.growth =im{2}==1 & im{1}==0;
 d.shrink =im{1}==1 & im{2}==0;
+imagesc(d.growth); axis image; title('Growth')
+figure; imagesc(d.shrink); axis image; title('Shrink')
 
+%% stats
 stats=regionprops(d.max_water, 'Area', 'Perimeter', 'PixelIdxList', 'Centroid')
+
+for i=1:length(stats)
+    disp(i)
+    stats(i).change=sum(d.growth(stats(i).PixelIdxList))-...
+        sum(d.shrink(stats(i).PixelIdxList));
+    stats(i).size.a=sum(im{1}(stats(i).PixelIdxList)==1);
+    stats(i).size.b=sum(im{2}(stats(i).PixelIdxList)==1);
+end
